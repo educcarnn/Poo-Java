d61c50ec6f38e4eb589d7c887314753d89189cac
educcarnn <educcarnn@gmail.com>
2023-11-22 16:53:53 -0300

    feat: class main

--- src/pacote/Funcionario.java
+++ src/pacote/Funcionario.java
@@ -1,4 +1,4 @@
-package pacote;  // Lembre-se de ajustar conforme a estrutura do seu pacote
+package pacote;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
@@ -9,29 +9,29 @@ public abstract class Funcionario {
     protected String cargo;
     protected LocalDate dataContratacao;
 
-    // Construtor
+
     public Funcionario(String nome, String cargo, String dataContratacao) {
         this.nome = nome;
         this.cargo = cargo;
         this.dataContratacao = parseData(dataContratacao);
     }
 
-    // Método para analisar a data no formato desejado
+
     private LocalDate parseData(String data) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
         return YearMonth.parse(data, formatter).atDay(1);
     }
 
-    // Método para calcular o número de anos de serviço
+
     protected long calcularAnosDeServico(int mes, int ano) {
         LocalDate dataAtual = LocalDate.of(ano, mes, 1);
         return java.time.temporal.ChronoUnit.YEARS.between(dataContratacao, dataAtual);
     }
 
-    // Método abstrato para calcular salário (será implementado nas subclasses)
+
     public abstract double calcularSalario(int mes, int ano);
 
-    // Getters
+
     public String getNome() {
         return nome;
     }
