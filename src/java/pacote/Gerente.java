92e7d38fd7cafddf6172d9ba0cc987b8580d6732
educcarnn <educcarnn@gmail.com>
2023-11-22 16:12:37 -0300

    feat: vendedor class and gerente

--- src/java/pacote/Gerente.java
+++ src/java/pacote/Gerente.java
@@ -1,4 +1,14 @@
 package java.pacote;
 
-public class Gerente {
+import java.time.LocalDate;
+
+public class Gerente extends Funcionario {
+
+    public Gerente(String nome, String dataContratacao) {
+        super(nome, "Gerente", dataContratacao);
+    }
+    @Override
+    public double calcularSalario(int mes, int ano) {
+        return 20000.0;
+    }
 }
--- src/java/pacote/Vendedor.java
+++ src/java/pacote/Vendedor.java
@@ -1,4 +1,33 @@
 package java.pacote;
 
-public class Vendedor {
+import java.time.LocalDate;
+
+public class Vendedor extends Funcionario {
+
+    private double valorVendido;
+
+    public Vendedor(String nome, String dataContratacao) {
+        super(nome, "Vendedor", dataContratacao);
+        this.valorVendido = 0.0;
+    }
+
+
+    public void registrarVenda(double valor) {
+        this.valorVendido += valor;
+    }
+
+    @Override
+    public double calcularSalario(int mes, int ano) {
+
+        double salarioBase = 12000.0;
+
+
+        long anosDeServico = calcularAnosDeServico(mes, ano);
+        double bonusAnosServico = anosDeServico * 1800.0;
+
+        double bonusVendas = valorVendido * 0.3;
+
+
+        return salarioBase + bonusAnosServico + bonusVendas;
+    }
 }
